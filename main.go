907fe7d90698a7eda530e22cf6a5cf29ec3ad7fc
= <156850571+sudo-haggis@users.noreply.github.com>
2025-06-03 17:38:57 +0200

    bugfix(main_json): fixxed bug for gosec

--- main.go
+++ main.go
@@ -90,15 +90,13 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 
 	router.Mount("/v1", v1Router)
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
-	}
-
 	log.Printf("Serving on port: %s\n", port)
-	srv = &http.Server{
+	srv := &http.Server{
 		Addr:              ":" + port,
 		Handler:           router,
 		ReadHeaderTimeout: time.Second * 5,
 	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
